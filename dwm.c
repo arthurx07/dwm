2a599d52783a78d64074fe7a37342c29b41acdce
kumao <arturleivarx@gmail.com>
2023-09-06 00:57:39 +0200

    apply 17-dwm-floating-statusbar-20230906.diff

--- dwm.c
+++ dwm.c
@@ -1060,7 +1060,6 @@ void
 incnmaster(const Arg *arg)
 {
 	selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag] = MAX(selmon->nmaster + arg->i, 0);
-	arrange(selmon);
 }
 
 #ifdef XINERAMA
@@ -2113,9 +2112,7 @@ updatebarpos(Monitor *m)
 	m->wy = m->my;
 	m->wh = m->mh;
 	if (m->showbar) {
-		m->wh = m->wh - vertpad - bh;
 		m->by = m->topbar ? m->wy : m->wy + m->wh + vertpad;
-		m->wy = m->topbar ? m->wy + bh + vp : m->wy;
 	} else
 		m->by = -bh - vp;
 }
