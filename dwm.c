c27ee79c043a0aeb796546c8d3bd6865274fb144
kumao <arturleivarx@gmail.com>
2023-09-06 20:17:33 +0200

    apply 18-dwm-actualfullscreen-20211013-cb3f58a.diff

--- dwm.c
+++ dwm.c
@@ -243,6 +243,7 @@ static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void togglesticky(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -1977,6 +1978,13 @@ togglesticky(const Arg *arg)
 		return;
     setsticky(selmon->sel, !selmon->sel->issticky);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
