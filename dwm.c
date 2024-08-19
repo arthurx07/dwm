6bbe44ae0ea690fc0018cac54d538b6d7933b520
kumao <arturleivarx@gmail.com>
2024-08-19 17:31:43 +0200

    apply 16-dwm-alwayscenter-20200625-f04cac6.diff

--- dwm.c
+++ dwm.c
@@ -1994,7 +1994,6 @@ togglebar(const Arg *arg)
 	selmon->showbar = !selmon->showbar;
 	updatebarpos(selmon);
 	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx + sp, selmon->by + vp, selmon->ww - 2 * sp, bh);
-	arrange(selmon);
 }
 
 void
@@ -2157,9 +2156,7 @@ updatebarpos(Monitor *m)
 	m->wy = m->my;
 	m->wh = m->mh;
 	if (m->showbar) {
-		m->wh = m->wh - vertpad - bh;
 		m->by = m->topbar ? m->wy : m->wy + m->wh + vertpad;
-		m->wy = m->topbar ? m->wy + bh + vp : m->wy;
 	} else
 		m->by = -bh - vp;
 }
